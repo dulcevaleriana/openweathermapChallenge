46d655bf87b6def2cf9bcbc5d2b3ca816e296433
dulce valeria guzman taveras <Blastomm@hotmail.com>
2021-10-21 17:15:20 -0400

    view about us us done

--- src/view/aboutUs.tsx
+++ src/view/aboutUs.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Card from '../components/card';
+import SunAnimated from '../components/sunAnimated';
 
 const AboutUs = () => {
     return(
-        <>
-        <h1>AboutUs</h1>
-        </>
+        <div className="class-AboutUs class-ContactUs">
+            <h1>About OWPC (openweathermapchallenge)</h1>
+            <Card 
+                mapData={[{
+                    value: "This is a repository to complete a challenge which consists of applying the api called openweathermap, which is used to consult the climate status of each country."
+                }]}
+            />
+            <SunAnimated/>
+        </div>
     )
 }
 
