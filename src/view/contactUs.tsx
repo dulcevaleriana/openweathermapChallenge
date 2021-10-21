fd1a60c7ca96a96134042f686107f3c40275784e
dulce valeria guzman taveras <Blastomm@hotmail.com>
2021-10-21 17:07:47 -0400

    view contact us done

--- src/view/contactUs.tsx
+++ src/view/contactUs.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import SunAnimated from "../components/sunAnimated";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const ContactUs = () => {
     return(
-        <h1>contactUs</h1>
+        <div className="class-ContactUs">
+            <h1>contactUs</h1>
+            <form>
+                <input type="text" placeholder="Write your name"/>
+                <input type="email" placeholder="Write your email"/>
+                <div>
+                    <textarea rows={3} placeholder="Write your message"/>
+                    <button type="button">
+                        <FontAwesomeIcon icon={faPaperPlane} /> <span>Send</span>
+                    </button>
+                </div>
+            </form>
+            <SunAnimated/>
+        </div>
     )
 }
 
