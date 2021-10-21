3d5a10a47fcdfff0bbaeb8083e905d1dc558f260
dulce valeria guzman taveras <Blastomm@hotmail.com>
2021-10-21 11:26:01 -0400

    footer completed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import IMG_MOUNTAIN from '../assets/img/background_mountain.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import FooterMountain from "./footerMountain";
 
 const Footer = () => {
     return(
         <div className="class-footer">
-            <img src={IMG_MOUNTAIN} alt="IMG_MOUNTAIN"/>
+            <FooterMountain />
             <div><h6>Make whit <FontAwesomeIcon icon={faHeart} /> by Dulce Valeriana</h6></div>
         </div>
     )
---
+++ src/components/footerMountain.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FooterMountain = () => {
+    return(
+        <>
+            <div className="class-sand-blue"/>
+            <div className="class-mountain class-big-mountain"/>
+            <div className="class-mountain class-big-mountain"/>
+            <div className="class-mountain class-big-mountain"/>
+            <div className="class-mountain class-big-mountain"/>
+            <div className="class-mountain class-small-mountain"/>
+            <div className="class-mountain class-small-mountain"/>
+            <div className="class-sand-light-blue"/>
+        </>
+    )
+}
+
+export default FooterMountain;
