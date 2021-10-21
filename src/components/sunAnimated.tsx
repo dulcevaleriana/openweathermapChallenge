2585d97af7d8c441888c925d0024fef77e85d82c
dulce valeria guzman taveras <Blastomm@hotmail.com>
2021-10-21 13:47:02 -0400

    component sun done

--- src/components/sunAnimated.tsx
+++ src/components/sunAnimated.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SunAnimated = () =>{
     return(
         <div className="class-SunAnimated">
-            <div className="class-cloud class-cloud-big"/>
+            <div className="class-cloud class-cloud-small"/>
             <div className="class-sun"/>
             <div className="class-cloud class-cloud-small"/>
         </div>
