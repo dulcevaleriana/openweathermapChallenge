71af1ee371430e8176bd322632a6efdb33b9794d
dulce valeria guzman taveras <Blastomm@hotmail.com>
2021-10-21 10:02:53 -0400

    save img

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import IMG_MOUNTAIN from '../assets/img/background_mountain.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return(
-        <div>Footer</div>
+        <div className="class-footer">
+            <img src={IMG_MOUNTAIN} alt="IMG_MOUNTAIN"/>
+            <div><h6>Make whit <FontAwesomeIcon icon={faHeart} /> by Dulce Valeriana</h6></div>
+        </div>
     )
 }
 export default Footer;
